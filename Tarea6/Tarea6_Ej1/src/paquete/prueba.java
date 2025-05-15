2c8926871b0e583eecf8edfc6b51bbfcbb6f7e57
Cecchi08 <163585366+Cecchi08@users.noreply.github.com>
2025-05-15 14:32:03 -0300

    Add files via upload

---
+++ Tarea6/Tarea6_Ej1/src/paquete/prueba.java
@@ -0,0 +1,8 @@
+package paquete;
+
+public class prueba {
+	public static void main(String[] args) {
+		Automovil auto = new Automovil();
+		System.out.println(auto.toString());
+	}
+}
---
+++ Tarea6/Tarea6_Ej2/src/paquete/prueba.java
@@ -0,0 +1,15 @@
+package paquete;
+
+public class prueba {
+	  public static void main(String[] args) {
+		  Equipo equipo = new Equipo();
+		  Jugador jugador = new Jugador("mateo", 17, 10, 10);
+		  equipo.toString();
+		  jugador.toString();
+		  equipo.agregar("mateo", 17, 10, 10);
+		  equipo.sacar("mateo", 17, 10, 10);
+		  equipo.agregar("nene", 31, 7, 10);
+		  equipo.listar();
+		  System.out.println( "La cantidad de jugadores que tiene le club es " + equipo.cantidadJugadores());
+	  }
+}
